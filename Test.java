ca041b27f787a9bf89534b9c0517f29925fcdc49
jarodzhao <3245121@qq.com>
2018-03-14 23:19:10 +0800

    TestWhile 作业

--- Test.java
+++ Test.java
@@ -4,32 +4,17 @@ class Test
 {
 	public static void main(String[] args)
 	{
-		//Random rand = new Random();
-		//print(3 + 1.5);	// 自动转换为 Double 类型
-
-		while(condition()){
-			print("---------------------------");
-		}
-		print("Exited 'while'");
-	}
-
-	// 很巧妙的方法
-	static boolean condition()
-	{
-		double i = Math.random();
-		print(i);
-		return i < 0.99;
-	}
-
-	// 简便的输出方法
-	static void print(Object o)
-	{
-		// 判断对象是否为 String 类型
-		if (o instanceof String)
-			System.out.println(o);
-		else {
-			System.out.println(o.toString());
-			System.out.println(o.getClass().toString());
+		int r0 = 0;
+		for(;;){
+			int r = (int)(Math.random()*25);
+			if (r > r0)
+			{
+				System.out.print(r0 + " < " + r);
+			}else{
+				System.out.print(r0 + " > " + r);
+			}
+			System.out.print(" | ");
+			r0 = r;
 		}
 	}
 }
