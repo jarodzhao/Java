e75f1be8c784c4be1de2d56bf5569415193049ae
jarodzhao <3245121@qq.com>
2018-03-14 22:44:06 +0800

    巧妙的 while 循环

--- Test.java
+++ Test.java
@@ -1,10 +1,35 @@
-import java.util.Random;
+// import java.util.Random;
 
 class Test
 {
 	public static void main(String[] args)
 	{
-		Random rand = new Random();
-		System.out.println(rand.nextFloat());
+		//Random rand = new Random();
+		//print(3 + 1.5);	// 自动转换为 Double 类型
+
+		while(condition()){
+			print("---------------------------");
+		}
+		print("Exited 'while'");
+	}
+
+	// 很巧妙的方法
+	static boolean condition()
+	{
+		double i = Math.random();
+		print(i);
+		return i < 0.99;
+	}
+
+	// 简便的输出方法
+	static void print(Object o)
+	{
+		// 判断对象是否为 String 类型
+		if (o instanceof String)
+			System.out.println(o);
+		else {
+			System.out.println(o.toString());
+			System.out.println(o.getClass().toString());
+		}
 	}
 }
--- dr.java
+++ dr.java
@@ -2,6 +2,7 @@ class dr
 {
 	public static void main(String[] args) 
 	{
+		// 一道面试题，关于整除
 		int n = 789;
 		System.out.println(n / 100 * 100);
 		System.out.println(4 | 3);
