8061cc3f92c528cb91e936c91e5a4f7cf2e66ea7
jarodzhao <3245121@qq.com>
2018-03-15 15:00:59 +0800

    toUpperCase

--- Test.java
+++ Test.java
@@ -1,20 +1,10 @@
-// import java.util.Random;
 
 class Test
 {
 	public static void main(String[] args)
 	{
-		int r0 = 0;
-		for(;;){
-			int r = (int)(Math.random()*25);
-			if (r > r0)
-			{
-				System.out.print(r0 + " < " + r);
-			}else{
-				System.out.print(r0 + " > " + r);
-			}
-			System.out.print(" | ");
-			r0 = r;
-		}
+		String a = "console";
+
+		System.out.print(a.toUpperCase());
 	}
 }
