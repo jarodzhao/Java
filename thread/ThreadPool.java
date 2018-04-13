53d18f36f674237e8542d0096ec324a148ffa047
jarodzhao <3245121@qq.com>
2018-04-13 22:21:31 +0800

    可以返回结果的线程

--- thread/ThreadPool.java
+++ thread/ThreadPool.java
@@ -5,10 +5,15 @@ public class ThreadPool
 	public static void main(String[] args) 
 	{
 		// 线程（执行）管理器
-		// ExecutorService exec = Executors.newCachedThreadPool();
 
-		// 启用 5 个线程同时并发
-		ExecutorService exec = Executors.newFixedThreadPool(3);
+		// 总是启用新线程
+		ExecutorService exec = Executors.newCachedThreadPool();
+
+		// 总是使用同一线程（队列执行任务）
+		// ExecutorService exec = Executors.newSingleThreadExecutor();
+
+		// 启用 5 个线程同时并发（固定线程数量）
+		// ExecutorService exec = Executors.newFixedThreadPool(3);
 		
 		// 5 个任务
 		for(byte i=0;i<5;i++)
---
+++ thread/ThreadWithResult.java
@@ -0,0 +1,52 @@
+import java.util.concurrent.*;
+import java.util.*;
+
+class ThreadWithResult
+{
+	public static void main(String[] args){
+		ExecutorService exec = Executors.newCachedThreadPool();
+		ArrayList<Future<String>> results = new ArrayList<>();
+
+		// 循环 10 次，把 i 值传入新线程中
+		for(byte i=0;i<10;i++)
+
+			// submit 方法执行线程，并返回线程执行的结果
+			results.add(exec.submit(new CallableDemo(i)));
+
+		// 遍历 results ，输出其中的所有值（Future）
+		for(Future<String> fs:results){
+			try
+			{
+				System.out.println(fs.get());
+			}
+			catch (InterruptedException e)
+			{
+				System.out.println(e);
+				return;
+			}
+			catch (ExecutionException e)
+			{
+				System.out.println(e);
+			}
+			finally
+			{
+				exec.shutdown();
+			}
+		}
+	}
+}
+
+class CallableDemo implements Callable<String> 
+{
+	private int id;
+	
+	// 构造线程时接收一个参数 id
+	public CallableDemo(int id){
+		this.id = id;
+	}
+
+	// 输出线程构造实例时接收的 id
+	public String call(){
+		return id + " thread...";
+	}
+}
